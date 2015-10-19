3f32666477f7b516c1fc82adc85c8f1005e8d1c5
mushfique <ar.mushfique@gmail.com>
2015-10-19 20:10:49 +1100

    testcase

--- TouristTests/MapViewViewModelTest.m
+++ TouristTests/MapViewViewModelTest.m
@@ -12,16 +12,36 @@
 
 @end
 
-@implementation MapViewViewModelTest
+@implementation MapViewViewModelTest{
+    MapViewViewModel *vm;
+}
 
 - (void)setUp {
     [super setUp];
+    
+    vm = [[MapViewViewModel alloc]init];
+    
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+}
+
+-(void)testPlaces{
+    
+    EXP_START(@"testPlaces");
+    [vm handleLocationUpdateForLocation:CLLocationCoordinate2DMake(-37.8380298, 144.9911135)];
+    __block id places = nil;
+    [self.KVOController observe:vm keyPath:@"places" options:NSKeyValueObservingOptionNew block:^(id observer, id object, NSDictionary *change) {
+        places = change[NSKeyValueChangeNewKey];
+        
+        XCTAssertNotNil(places, @"places came nil");
+        
+        EXP_FULFILL();
+    }];
+    EXP_END();
 }
 
 
